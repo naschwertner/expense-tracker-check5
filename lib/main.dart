d05dc92b4bcf1ef1bf249baf29cac5fbb936f34d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-16 20:39:24 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/pages/conta_cadastro_page.dart';
+import 'package:expense_tracker/pages/home_page.dart';
+import 'package:expense_tracker/pages/login_page.dart';
+import 'package:expense_tracker/pages/registar_page.dart';
+import 'package:expense_tracker/pages/splash_page.dart';
+import 'package:expense_tracker/pages/transacao_cadastro_page.dart';
+import 'package:expense_tracker/pages/transacao_detalhes_page.dart';
+
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  await Supabase.initialize(
+    url: 'https://zjbopxzhfbrxgzeobhdt.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqYm9weHpoZmJyeGd6ZW9iaGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3NzQyMjUsImV4cCI6MjAxMTM1MDIyNX0.FGX3AyIuD1fzk9PJ3EOgXZSNwdhGxxlWGsxHX_wU4wo',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        "/": (context) => const HomePage(),
+        "/splash": (context) => const SplashPage(),
+        "/login": (context) => const LoginPage(),
+        "/registrar": (context) => const RegistrarPage(),
+        "/transacao-detalhes": (context) => const TransacaoDetalhesPage(),
+        "/transacao-cadastro": (context) => const TransacaoCadastroPage(),
+        "/conta-cadastro": (context) => const ContaCadastroPage(),
+      },
+      initialRoute: "/splash",
+    );
+  }
+}
